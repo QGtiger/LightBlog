344907a18a24cd1d9c0d79150220bc76a1718583
QGtiger <1426286337@qq.com>
2019-05-27 20:38:05 +0800

    博客首页优化

--- article/list_views.py
+++ article/list_views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render,get_object_or_404,HttpResponse
-from django.core.paginator import EmptyPage,Paginator,PageNotAnInteger
+from django.shortcuts import render, get_object_or_404, HttpResponse
+from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
 from django.core import serializers
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
-from .models import ArticlePost,Comment
+from .models import ArticlePost, Comment
 from comment.models import Comment_reply
 from django.conf import settings
 from .tasks import *
@@ -16,11 +16,17 @@ r = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT)
 
 
 def init_blog(content):
-    content_text1 = content.replace('<p>', '').replace('</p>', '').replace("'", '')
+    content_text1 = content.replace(
+        '<p>',
+        '').replace(
+        '</p>',
+        '').replace(
+            "'",
+        '')
     # 去掉图片链接
-    content_text2 = re.sub('(!\[.*?\]\(.*?\))', '', content_text1)
+    content_text2 = re.sub(r'(!\[.*?\]\(.*?\))', '', content_text1)
     # 去掉markdown标签
-    pattern = '[\\\`\*\_\[\]\#\+\-\!\>]'
+    pattern = r'[\\\`\*\_\[\]\#\+\-\!\>]'
     content_text3 = re.sub(pattern, '', content_text2)
     return content_text3
 
@@ -31,7 +37,7 @@ def article_titles(request):
     article_ranking_ids = [int(id) for id in article_ranking]
     most_viewed = list(ArticlePost.objects.filter(id__in=article_ranking_ids))
     most_viewed.sort(key=lambda x: article_ranking_ids.index(x.id))
-    return render(request,'article/article_titles.html',locals())
+    return render(request, 'article/article_titles.html', locals())
 
 
 def article_page(request):
@@ -49,28 +55,50 @@ def article_page(request):
         articles = current_page.object_list
     articles_json = []
     for i in range(len(articles)):
-        articles_json.append({'id':articles[i].id,'author':articles[i].author.username,'title':articles[i].title,'updated':time.mktime(articles[i].updated.timetuple()),'body': init_blog(articles[i].body[:150]), 'users_like':articles[i].users_like.count()})
-    #return HttpResponse(serializers.serialize("json",articles))
-    return HttpResponse(json.dumps({'static':200,'data':articles_json,'page_num':paginator.num_pages}))
+        view = r.get('article:{}:views'.format(articles[i].id))
+        if view == None:
+            view_count = 0
+        else:
+            view_count = view.decode('utf-8')
+        articles_json.append({'id': articles[i].id,
+                              'author': articles[i].author.username,
+                              'author_img_url': articles[i].author.userinfo.photo_150x150.url,
+                              'title': articles[i].title,
+                              'updated': time.mktime(articles[i].updated.timetuple()),
+                              'body': init_blog(articles[i].body[:200]),
+                              'users_like': articles[i].users_like.count(),
+                              'views': view_count})
+    # return HttpResponse(serializers.serialize("json",articles))
+    return HttpResponse(json.dumps(
+        {'static': 200, 'data': articles_json, 'page_num': paginator.num_pages}))
 
 
 @csrf_exempt
 def article_content(request, article_id):
     if request.method == 'POST':
         if request.user.username == "":
-            return HttpResponse(json.dumps({'code': 502, 'tips':'What are u doing now??'}))
+            return HttpResponse(json.dumps(
+                {'code': 502, 'tips': 'What are u doing now??'}))
         comment = request.POST.get('comment', '')
         if comment.strip() == '':
-            return HttpResponse(json.dumps({'code': 403, 'tips': '评论内容不能为空...'}))
+            return HttpResponse(json.dumps(
+                {'code': 403, 'tips': '评论内容不能为空...'}))
         try:
             user = request.user
             article = get_object_or_404(ArticlePost, id=article_id)
-            C = Comment(article=article,commentator=user,body=comment)
+            C = Comment(article=article, commentator=user, body=comment)
             C.save()
-            comment_info = {'commentator':user.username,'commentator_img_url':user.userinfo.photo_150x150.url, 'id': C.id, 'body': C.body, 'created': time.mktime(C.created.timetuple())}
-            return HttpResponse(json.dumps({"code":200,"tips":"感谢您的评论", 'comment_info':comment_info}))
-        except:
-            return HttpResponse(json.dumps({"code":501, "tips":"评论系统出现错误"}))
+            comment_info = {
+                'commentator': user.username,
+                'commentator_img_url': user.userinfo.photo_150x150.url,
+                'id': C.id,
+                'body': C.body,
+                'created': time.mktime(
+                    C.created.timetuple())}
+            return HttpResponse(json.dumps(
+                {"code": 200, "tips": "感谢您的评论", 'comment_info': comment_info}))
+        except BaseException:
+            return HttpResponse(json.dumps({"code": 501, "tips": "评论系统出现错误"}))
     else:
         article = get_object_or_404(ArticlePost, id=article_id)
         total_views = r.incr("article:{}:views".format(article_id))
@@ -83,21 +111,24 @@ def article_content(request, article_id):
 @csrf_exempt
 def comment_like(request):
     user = request.user
-    comment_id = request.POST.get("id","")
-    action = request.POST.get("action","")
+    comment_id = request.POST.get("id", "")
+    action = request.POST.get("action", "")
     if comment_id and action:
         try:
             comment = Comment.objects.get(id=comment_id)
             if action == 'like':
                 comment.comment_like.add(user)
                 num = comment.comment_like.count()
-                return HttpResponse(json.dumps({'static':200, 'support_num':num}))
+                return HttpResponse(json.dumps(
+                    {'static': 200, 'support_num': num}))
             else:
                 comment.comment_like.remove(user)
                 num = comment.comment_like.count()
-                return HttpResponse(json.dumps({'static':201, 'support_num':num}))
-        except:
-            return HttpResponse(json.dumps({'static':500,'tips':'系统错误,重新尝试'}))
+                return HttpResponse(json.dumps(
+                    {'static': 201, 'support_num': num}))
+        except BaseException:
+            return HttpResponse(json.dumps(
+                {'static': 500, 'tips': '系统错误,重新尝试'}))
 
 
 @csrf_exempt
@@ -108,11 +139,13 @@ def comment_delete(request):
     try:
         if request.user == comment.commentator:
             comment_delete_task.delay(comment_id)
-            return HttpResponse(json.dumps({'static':201, 'tips':'评论已删除'}))
+            return HttpResponse(json.dumps({'static': 201, 'tips': '评论已删除'}))
         else:
-            return HttpResponse(json.dumps({'static':502, 'tips':"You don't have permission.."}))
-    except:
-        return HttpResponse(json.dumps({'static':500, 'tips':'Something Error...'}))
+            return HttpResponse(json.dumps(
+                {'static': 502, 'tips': "You don't have permission.."}))
+    except BaseException:
+        return HttpResponse(json.dumps(
+            {'static': 500, 'tips': 'Something Error...'}))
 
 
 def init_data(data):
@@ -120,20 +153,27 @@ def init_data(data):
     list_data = []
     for item in items:
         if item.reply_type == 0:
-            list_data.append({'from': item.comment_user.username,'from_img_url':item.comment_user.userinfo.photo_150x150.url, 'to':data.commentator.username , 'id': item.id, 'body': item.body if item.is_deleted is False else '评论已删除',
-                            'created': time.mktime(item.created.timetuple())})
+            list_data.append({'from': item.comment_user.username,
+                              'from_img_url': item.comment_user.userinfo.photo_150x150.url,
+                              'to': data.commentator.username,
+                              'id': item.id,
+                              'body': item.body if item.is_deleted is False else '评论已删除',
+                              'created': time.mktime(item.created.timetuple())})
         else:
             to_id = item.reply_comment
-            list_data.append(
-                {'from': item.comment_user.username,'from_img_url':item.comment_user.userinfo.photo_150x150.url, 'to': Comment_reply.objects.get(id=to_id).comment_user.username,'id': item.id, 'body': item.body if item.is_deleted is False else '评论已删除',
-                 'created': time.mktime(item.created.timetuple())})
+            list_data.append({'from': item.comment_user.username,
+                              'from_img_url': item.comment_user.userinfo.photo_150x150.url,
+                              'to': Comment_reply.objects.get(id=to_id).comment_user.username,
+                              'id': item.id,
+                              'body': item.body if item.is_deleted is False else '评论已删除',
+                              'created': time.mktime(item.created.timetuple())})
     return list_data
 
 
 def comment_page(request, article_id):
     article = get_object_or_404(ArticlePost, id=article_id)
     comments_all = article.comments.all()
-    paginator = Paginator(comments_all,6)
+    paginator = Paginator(comments_all, 6)
     page = request.GET['page']
     try:
         current_page = paginator.page(page)
@@ -146,5 +186,12 @@ def comment_page(request, article_id):
         comments = current_page.object_list
     comments_list = []
     for item in comments:
-        comments_list.append({'id': item.id, 'commentator': item.commentator.username,'commentator_img_url': item.commentator.userinfo.photo_150x150.url, 'comment_reply':init_data(item), 'created': time.mktime(item.created.timetuple()), 'comment_like': item.comment_like.count(), 'body': item.body if item.is_deleted is False else '评论已删除'})
-    return HttpResponse(json.dumps({'code':200, 'res': comments_list, 'page_num': paginator.num_pages}))
+        comments_list.append({'id': item.id,
+                              'commentator': item.commentator.username,
+                              'commentator_img_url': item.commentator.userinfo.photo_150x150.url,
+                              'comment_reply': init_data(item),
+                              'created': time.mktime(item.created.timetuple()),
+                              'comment_like': item.comment_like.count(),
+                              'body': item.body if item.is_deleted is False else '评论已删除'})
+    return HttpResponse(json.dumps(
+        {'code': 200, 'res': comments_list, 'page_num': paginator.num_pages}))
